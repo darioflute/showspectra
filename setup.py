78dbb03c69a25b385a20fd1bdabb2e387ef2c2e5
Dario Fadda <darioflute@gmail.com>
2018-09-21 09:48:52 -0700

    version 0.07

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 config = {
     'name': 'showspectra',
-    'version': '0.06',
+    'version': '0.07',
     'description': 'Show Spectra',
     'long_description': 'The package shows spectra and fits lines',
     'author': 'Dario Fadda',
